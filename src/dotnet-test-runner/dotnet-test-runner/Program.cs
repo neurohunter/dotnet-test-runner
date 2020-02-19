d42c056c180452160561e3b044e11868ffd1dae6
Pavel Velerjevich Vishnyakov <pavel.vishnyakov@sioux.eu>
2020-02-19 09:45:47 +0100

    add initial project

---
+++ src/dotnet-test-runner/dotnet-test-runner/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.CommandLine;
+
+namespace Dotnet.Test.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
